9418e39a7dc7ec8198b41e9a5a1e6fda3a9d3c9e
Mykhailo <hryvinskyim@gmail.com>
2025-04-23 15:49:43 +0300

    initial commit

---
+++ reactOptics/src/App.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const MainHeader = styled.h1`
+    display: block;
+    padding: 10px;
+    color: blue;
+    border-radius: 8px 0 8px 0;
+    border: 2px solid currentColor;
+    margin: 0 auto;
+`;
---
+++ reactOptics/src/App.jsx
@@ -0,0 +1,12 @@
+
+import { MainHeader } from './App';
+
+const App = () => {
+  return (
+    <>
+      <MainHeader>Привіт</MainHeader>
+    </>
+  )
+}
+
+export default App
